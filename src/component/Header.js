f2513bef5baa1e18f53b630a99e5d9c47b0487b4
ASUS\gkrua <gkrua5056@naver.com>
2024-10-30 14:07:42 +0900

    포트폴리오 사이트 제작 시작

---
+++ src/component/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/Header.css";
+
+function Header() {
+
+return (
+ <div className="Container">
+
+ 안녕하세요!
+
+ </div>
+
+
+
+);
+
+}
+
+export default Header;
